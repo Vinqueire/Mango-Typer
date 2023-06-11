53e16594ce45fc3b79fa325ded50f28d9c6fd557
Vinqueire <Frolof192@gmail.com>
2023-06-11 12:44:26 +0200

    Idk, I preety much made the js for colours

--- static/displayer.js
+++ static/displayer.js
@@ -1,8 +1,24 @@
 function putText(){
-    if (document.getElementById("timeSelect").value == 0){
+    if (document.getElementById("timeSelect").value === 0){
         alert("Thing not selected");
         
     }
-    else
+    else{
         getTime();
+        const texts = ["This is text nr 1 to fill", "This is text nr 2 to fill", "This is text nr 3 to fill"];
+        var randomNum = Math.floor(Math.random() * 3)
+        document.getElementById("stuffToType").textContent = texts[randomNum];
+    }
+}
+
+// This asshole shit doesnt work, textarea doesnt work for changing a single letter, figure out how to make this shit work It for sure won't work with textarea
+function checkText(){
+    const randText = document.getElementById("stuffToType");
+    console.log(randText).textContent;
+    var inputedText = document.getElementById("myText").value;
+    for (var i = 0; i < randText.length; i++){
+        temp = randText[i];
+        if (randText[i] === inputedText[i])
+        document.getElementById("stuffToType")[i].style.backgroundColor = 'blue';
+    }
 }
