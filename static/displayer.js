fcaab2dee7f287417787ed7fefd271202274eb27
Vinqueire <Frolof192@gmail.com>
2023-06-12 13:13:38 +0200

    This works temporarily

--- static/displayer.js
+++ static/displayer.js
@@ -1,14 +1,41 @@
-function putText(){
-    if (document.getElementById("timeSelect").value === 0)
-        alert("No time has been selected");
-    else
-        getTime();
-}
-
-
-function checkText(){
-    var randText = document.getElementById("stuffToType").innerHTML;
-    var inputedText = document.getElementById("myText").value;
-    
-    // This needs a rework, It's suppoused to make the ui background colour green/red on inputed text
-} 
+function displayText(challengeText) {
+    let challengeTextContainer = document.getElementById("challengeText");
+    challengeTextContainer.innerHTML = "";
+  
+    for (let i = 0; i < challengeText.length; i++) {
+      let span = document.createElement("span");
+      span.textContent = challengeText[i];
+  
+      challengeTextContainer.appendChild(span);
+    }
+  }
+  
+  document.getElementById("userInput").addEventListener("input", function () {
+    const challengeText = document.getElementById("challengeText").textContent;
+    const userInput = document.getElementById("userInput").value;
+    const userInputLength = userInput.length;
+  
+    let challengeTextContainer = document.getElementById("challengeText");
+    challengeTextContainer.innerHTML = "";
+  
+    for (let i = 0; i < challengeText.length; i++) {
+      let span = document.createElement("span");
+      span.textContent = challengeText[i];
+  
+      if (i < userInputLength) {
+        if (userInput[i] === challengeText[i]) {
+          span.style.backgroundColor = "green";
+        } else {
+          span.style.backgroundColor = "red";
+        }
+      }
+  
+      challengeTextContainer.appendChild(span);
+    }
+  
+    if (userInput === challengeText) {
+      // Handle completion of challenge
+      alert("Challenge completed!");
+    }
+  });
+  
