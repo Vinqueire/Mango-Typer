604483a768e0cd03aebb79e2a494c48cc5c6efe2
Vinqueire <Frolof192@gmail.com>
2023-06-14 12:21:15 +0200

    optimized the displayer

--- countdown.js
+++ countdown.js
@@ -3,7 +3,10 @@ function countdown(timeVal)
     document.getElementById("countdown").removeAttribute("hidden");
     document.getElementById("begin").setAttribute("hidden", "true");
     if (timeVal == 0)
+    {
         startTest();
+        displayText(document.getElementById("challengeText").textContent);
+    }
     else 
     {
     document.getElementById("countdownTime").textContent = timeVal;
--- displayer.js
+++ displayer.js
@@ -1,57 +1,59 @@
+// Create span for each
+let spanContainer = [];
 function displayText(challengeText) {
     let container = document.getElementById("challengeText");
     container.innerHTML = "";
-  
+
     for (let i = 0; i < challengeText.length; i++) {
       let span = document.createElement("span");
       span.textContent = challengeText[i];
   
       container.appendChild(span);
+      spanContainer.push(span);
     }
   }
   
-  document.getElementById("userInput").addEventListener("input", function () {
-    const challengeText = document.getElementById("challengeText").textContent;
-    const userInput = document.getElementById("userInput").value;
-    const userInputLength = userInput.length;
-  
-    let container = document.getElementById("challengeText");
-    container.innerHTML = "";
+document.getElementById("userInput").addEventListener("input", function () {
+  const challengeText = document.getElementById("challengeText").textContent;
+  const userInput = document.getElementById("userInput").value;
+  const userInputLength = userInput.length;
 
-    var color = document.getElementById("color").value;
+  let container = document.getElementById("challengeText");
+  container.innerHTML = "";
 
-    var clr1 = "rgb(20, 184, 55)";
-    var clr2 = "rgb(201, 45, 17)";
+  var color = document.getElementById("color").value;
 
-    if (color == 1)
-    {
-      clr1 = "rgb(20, 184, 55)";
-      clr2 = "red";
+  var clr1 = "rgb(20, 184, 55)";
+  var clr2 = "rgb(201, 45, 17)";
+
+  if (color == 1)
+  {
+    clr1 = "rgb(20, 184, 55)";
+    clr2 = "red";
+  }
+  else if (color == 2)
+  {
+    clr1 = "rgb(24, 164, 199)";
+    clr2 = "orange";
+  }
+  else if (color == 3)
+  {
+    clr1 = "rgb(164, 17, 201)";
+    clr2 = "rgb(20, 184, 55)";
+  }
+
+  for (let i = 0; i < challengeText.length; i++) {
+    let span = spanContainer[i];
+    span.textContent = challengeText[i];
+
+    if (i < userInputLength) {
+      if (userInput[i] === challengeText[i]) {
+        span.style.backgroundColor = clr1;
+      } else 
+        span.style.backgroundColor = clr2;
     }
-    else if (color == 2)
-    {
-      clr1 = "rgb(24, 164, 199)";
-      clr2 = "orange";
-    }
-    else if (color == 3)
-    {
-      clr1 = "rgb(164, 17, 201)";
-      clr2 = "rgb(20, 184, 55)";
-    }
-  
-    for (let i = 0; i < challengeText.length; i++) {
-      let span = document.createElement("span");
-      span.textContent = challengeText[i];
-  
-      if (i < userInputLength) {
-        if (userInput[i] === challengeText[i]) {
-          span.style.backgroundColor = clr1;
-        } else {
-          span.style.backgroundColor = clr2;
-        }
-      }
-  
-      container.appendChild(span);
-    }
-  });
+
+    container.appendChild(span);
+  }
+});
   
